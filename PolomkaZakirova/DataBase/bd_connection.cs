d07b771ecb28adf54abaa04925c2cb408b153dde
kamil <kamil@DESKTOP-ST7GOA8>
2023-02-27 11:15:16 +0300

    Добавьте файлы проекта.

---
+++ PolomkaZakirova/DataBase/bd_connection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolomkaZakirova.DataBase
+{
+    public class bd_connection
+    {
+        public static PolomkaZakirovaEntities connection = new PolomkaZakirovaEntities();
+    }
+}
